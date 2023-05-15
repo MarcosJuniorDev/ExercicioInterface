4183fe9840714c020893297d2c7d434916c8d1d6
Marcos Junior Oliveira <marcosjoliveiradev@gmail.com>
2023-05-15 14:01:25 -0300

    primeira implementação basica

---
+++ ExerInterface/Entities/Contract.cs
@@ -0,0 +1,20 @@
+﻿
+namespace ExerInterface.Entities
+{
+    internal class Contract
+    {
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalValue { get; set; }
+        public List<Installment> Installments { get; set; } = new List<Installment>();
+
+        public Contract(int number, DateTime date, double totalValue)
+        {
+            Number = number;
+            Date = date;
+            TotalValue = totalValue;
+        }
+
+
+    }
+}
---
+++ ExerInterface/Entities/Installment.cs
@@ -0,0 +1,16 @@
+﻿namespace ExerInterface.Entities
+{
+    internal class Installment
+    {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+
+
+    }
+}
